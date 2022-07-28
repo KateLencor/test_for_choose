cce04047ca18afd172f04c9182347d306f674199
KateLencor <kate.lencor@gmail.com>
2022-07-29 01:13:38 +0300

    Финальный код

--- test_for_choose.cs
+++ test_for_choose.cs
@@ -1,1 +1,16 @@
-﻿
+﻿string[] Array;
+Console.WriteLine("Введите массив строк через пробел");
+string stroka = Console.ReadLine();
+Array = stroka.Split(' ');
+var result = new string[Array.Length];
+var i = 0;
+foreach (var element in Array)
+{
+    if (element.Length <= 3)
+    {
+        result[i] = element;
+        i++;
+    }
+}
+Console.WriteLine("Выборка элементов длинной до трех(включая) символов: ");
+Console.WriteLine(string.Join(Environment.NewLine, result, 0, i));
